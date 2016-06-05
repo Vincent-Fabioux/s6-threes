b2dc8be4adb57916281af86597af75c2466bf15b
Nicolas-Montoro <nicolas.montoro@u-psud.fr>
2016-06-05 09:36:43 +0200

    Update EndMenu.java

--- src/fr/fabiouxmontoro/threes/EndMenu.java
+++ src/fr/fabiouxmontoro/threes/EndMenu.java
@@ -1,25 +1,197 @@
-package fr.fabiouxmontoro.threes;
+package fr.polytech.ihm.advthrees;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class EndMenu extends JPanel
 {
 	private final static long serialVersionUID = 1L;
 
-	public final static int END_MENU_SIZE_X = 200;
-	public final static int END_MENU_SIZE_Y = 100;
+	public final static int END_MENU_SIZE_X = 300;
+	public final static int END_MENU_SIZE_Y = 400;
 	public final static int END_MENU_ROUND = 50;
-
-
-	public EndMenu()
+	
+	
+	
+	private int score;
+	private FileOutputStream fos=null;
+	private ArrayList<Integer> scores;
+	
+	private JLabel title;
+	private JLabel renderScore;
+	private JLabel[] ranking;
+	private JButton menu;
+	private JButton exit;
+	
+	public EndMenu(Threes threes)
 	{
+		title=new JLabel("Tableau des Scores");
+		renderScore=new JLabel();
+		renderScore=new JLabel();
+		menu=new JButton("Menu");
+		exit = new JButton("Quitter");
+		ranking=new JLabel[5];
+		
+		
 		this.setSize(END_MENU_SIZE_X, END_MENU_SIZE_Y);
 		this.setLocation((Threes.WINDOW_SIZE_X - END_MENU_SIZE_X)/2, (Threes.WINDOW_SIZE_Y - END_MENU_SIZE_Y)/2);
 		this.setBackground(new Color(255, 255, 255));
 		this.setOpaque(true);
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		menu.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
+		menu.setSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
+		menu.setBackground(Color.white);
+		menu.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{
+				threes.replay();
+			}
+				
+			@Override
+			public void mouseEntered(MouseEvent arg0) {}
+				
+			@Override
+			public void mouseExited(MouseEvent arg0) {}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {}
+		});
+			
+		exit.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
+		exit.setSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
+		exit.setBackground(Color.white);
+		exit.addMouseListener(new MouseListener(){
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{
+				threes.dispose();
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {}
+				
+		});
+		this.setSize(END_MENU_SIZE_X, END_MENU_SIZE_Y);
+		this.setLocation((Threes.WINDOW_SIZE_X - END_MENU_SIZE_X)/2, (Threes.WINDOW_SIZE_Y - END_MENU_SIZE_Y)/2);
+		this.setBackground(new Color(255, 255, 255));
+		this.setOpaque(true);
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		this.add(title);
+		this.add(renderScore);
+		
+		for(int i=0;i<5;i++){
+			ranking[i]=new JLabel();
+			this.add(ranking[i]);
+		}
+		
+		this.add(menu);
+		this.add(exit);
+
+	}
+	
+	public void writeFile(){
+		String result=Integer.toString(score);
+		result+=",";
+		try {
+			fos = new FileOutputStream(new File("scores.txt"),true);
+			try {
+				fos.write(result.getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        
+        } catch (FileNotFoundException e) {	
+			e.printStackTrace();
+		}finally{
+	        try {
+	            if (fos != null)
+	               fos.close();
+	         } catch (IOException e) {
+	            e.printStackTrace();
+	         }	
+		}
+		
+	}
+	
+	public void read(){
+		scores=new ArrayList<Integer>();
+		Scanner sc = null;
+		String temp="";
+		try {
+            try {
+                sc = new Scanner(new File("scores.txt"));
+                while (sc.hasNextLine()) {
+                	for (char c : sc.next().toCharArray()) {
+                		if(c!=','){
+                        	temp+=c;
+                        }
+                		else{
+                			scores.add(Integer.parseInt(temp));
+                			 Collections.sort(scores, Collections.reverseOrder());
+                			temp="";
+                		}
+                    }
+                }
+            } finally {
+                if (sc != null)
+                    sc.close();
+            }
+ 
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	}
+	
+	public void updateValues(){
+		renderScore.setText("Votre score est de "+Integer.toString(score));
+		for(int i=0;i<5;i++){
+			if(scores.get(i)!=null){
+				ranking[i].setText(Integer.toString(scores.get(i)));
+			}
+		}
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
