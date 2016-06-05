8994c10f1eac341b44740074663d064ba0579032
Nicolas-Montoro <nicolas.montoro@u-psud.fr>
2016-06-05 17:18:27 +0200

    Update MenuPanel.java

--- src/fr/fabiouxmontoro/threes/MenuPanel.java
+++ src/fr/fabiouxmontoro/threes/MenuPanel.java
@@ -1,100 +1,112 @@
 package fr.polytech.ihm.advthrees;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
-public class MenuPanel extends JPanel
-{
+public class MenuPanel extends JPanel {
 	private final static long serialVersionUID = 1L;
-	
-	private JPanel panel;
+
 	private JButton playBasic;
 	private JButton playAdvanced;
 	private JLabel titleImage;
+	@SuppressWarnings("unused")
 	private Threes threes;
-	
-	public MenuPanel(Threes threes)
-	{
+
+	public MenuPanel(Threes threes) {
 		this.threes = threes;
 
-		panel=new JPanel();
-		
-	    // Image du titre
-	    titleImage = new JLabel(new ImageIcon("threes.jpg"));
-	    titleImage.setIcon(new ImageIcon(new ImageIcon("threes.jpg").getImage().getScaledInstance(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y, Image.SCALE_DEFAULT)));
+		// Image du titre
+		titleImage = new JLabel(new ImageIcon("threes.jpg"));
+		titleImage.setIcon(
+				new ImageIcon(new ImageIcon("threes.jpg").getImage().getScaledInstance(340, 85, Image.SCALE_DEFAULT)));
 
-
-		// Bouton JOUER
-	    playBasic = new JButton("Version Basique");
-	    playBasic.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
-	    playBasic.setSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
-	    playBasic.setBackground(Color.white);
-	    playBasic.addMouseListener(new MouseListener()
-	    {
+		// Version Base
+		playBasic = new JButton("Version Basique");
+		playBasic.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
+		playBasic.setPreferredSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
+		playBasic.addMouseListener(new MouseListener() {
 			@Override
-			public void mouseClicked(MouseEvent arg0)
-			{
+			public void mouseClicked(MouseEvent arg0) {
 				threes.goToGame(0);
 			}
 
 			@Override
-			public void mouseEntered(MouseEvent arg0) {}
+			public void mouseEntered(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mouseExited(MouseEvent arg0) {}
+			public void mouseExited(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mousePressed(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mouseReleased(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {
+			}
 		});
-	    playAdvanced = new JButton("Version Avancée");
-	    playAdvanced.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
-	    playAdvanced.setSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
-	    playAdvanced.setBackground(Color.white);
-	    playAdvanced.addMouseListener(new MouseListener()
-	    {
+
+		// Version Avancee
+		playAdvanced = new JButton("Version Avancée");
+		playAdvanced.setFont(new Font("Arial", Font.BOLD, Threes.MAIN_BUTTONS_POLICE_SIZE));
+		playAdvanced.setPreferredSize(new Dimension(Threes.MAIN_BUTTONS_SIZE_X, Threes.MAIN_BUTTONS_SIZE_Y));
+		playAdvanced.addMouseListener(new MouseListener() {
 			@Override
-			public void mouseClicked(MouseEvent arg0)
-			{
+			public void mouseClicked(MouseEvent arg0) {
 				threes.goToGame(1);
 			}
 
 			@Override
-			public void mouseEntered(MouseEvent arg0) {}
+			public void mouseEntered(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mouseExited(MouseEvent arg0) {}
+			public void mouseExited(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mousePressed(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {
+			}
 
 			@Override
-			public void mouseReleased(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {
+			}
 		});
-	    
 
-	    // Fenêtre du menu
+		// Fenêtre du menu
 		setBackground(Color.white);
-		setSize(new Dimension(Threes.WINDOW_SIZE_X, Threes.WINDOW_SIZE_Y));
-        panel.add(titleImage, BorderLayout.NORTH);
-        panel.add(playAdvanced,BorderLayout.CENTER);
-        panel.add(playBasic, BorderLayout.CENTER);
-        panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
-        add(panel,BorderLayout.SOUTH);
+
+		//Mise en place des composants
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.weighty = 0.5;
+		constraints.anchor = GridBagConstraints.NORTH;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add(titleImage, constraints);
+
+		constraints.anchor = GridBagConstraints.NORTH;
+		constraints.gridy = 1;
+		this.add(playBasic, constraints);
+
+		constraints.gridy = 2;
+		this.add(playAdvanced, constraints);
+
 	}
+
 }
+
