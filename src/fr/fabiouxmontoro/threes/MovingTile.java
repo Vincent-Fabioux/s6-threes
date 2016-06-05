914a15f4a5244ede3a3e4a87503d72b2953e628f
Nicolas-Montoro <nicolas.montoro@u-psud.fr>
2016-06-05 17:20:12 +0200

    Update MovingTile.java

--- src/fr/fabiouxmontoro/threes/MovingTile.java
+++ src/fr/fabiouxmontoro/threes/MovingTile.java
@@ -1,92 +1,80 @@
-package fr.fabiouxmontoro.threes;
+package fr.polytech.ihm.advthrees;
 
-public class MovingTile extends Tile
-{
-	public final static int TILES_SPEED_X = TILES_SIZE_X / 5;		// Vitesse X des tuiles (pixels/frame)
-	public final static int TILES_SPEED_Y = TILES_SIZE_Y / 5;		// Vitesse Y des tuiles (pixels/frame)
-	
+public class MovingTile extends Tile {
+	public final static int TILES_SPEED_X = TILES_SIZE_X / 5; // Vitesse X des
+																// tuiles
+																// (pixels/frame)
+	public final static int TILES_SPEED_Y = TILES_SIZE_Y / 5; // Vitesse Y des
+																// tuiles
+																// (pixels/frame)
+
 	private boolean isMoving;
 	private int movementX;
 	private int movementY;
 	private int targetPos;
-	
-	public MovingTile(int posx, int posy, int targetPos)
-	{
+
+	public MovingTile(int posx, int posy, int targetPos) {
 		super(posx, posy);
 		movementX = 0;
 		movementY = 0;
 		this.targetPos = targetPos;
 	}
-	
-	public void setMove(int posx, int posy)
-	{
+
+	public void setMove(int posx, int posy) {
 		isMoving = true;
 		movementX = posx;
 		movementY = posy;
 	}
-	
-	
+
 	/**
 	 * Déplace la tuile vers une position donnée
-	 * @param posX position X
-	 * @param posY position Y
+	 * 
+	 * @param posX
+	 *            position X
+	 * @param posY
+	 *            position Y
 	 */
-	public void moveTo(int posX, int posY)
-	{
+	public void moveTo(int posX, int posY) {
 		shape.setFrame(posX, posY, shape.getWidth(), shape.getHeight());
 	}
 
-	
 	/**
 	 * Permet de savoir si la tuile est en mouvement
+	 * 
 	 * @return mouvement ou non
 	 */
-	public boolean isMoving()
-	{
+	public boolean isMoving() {
 		return isMoving;
 	}
-	
-	
-	public int getTargetPos()
-	{
+
+	public int getTargetPos() {
 		return targetPos;
 	}
-	
-	
+
 	/**
 	 * Bouge la tuile si un mouvement à été initié
 	 */
-	public void move()
-	{
-		if(movementX > TILES_GAP)
-		{
+	public void move() {
+		if (movementX > TILES_GAP) {
 			moveTo((int) shape.getX() - TILES_SPEED_X, (int) shape.getY());
 			movementX -= TILES_SPEED_X;
-		}
-		else if(movementX < -1*TILES_GAP)
-		{
+		} else if (movementX < -1 * TILES_GAP) {
 			moveTo((int) shape.getX() + TILES_SPEED_X, (int) shape.getY());
 			movementX += TILES_SPEED_X;
-		}
-		else if(movementY > TILES_GAP)
-		{
+		} else if (movementY > TILES_GAP) {
 			moveTo((int) shape.getX(), (int) shape.getY() - TILES_SPEED_Y);
 			movementY -= TILES_SPEED_Y;
-		}
-		else if(movementY < -1*TILES_GAP)
-		{
+		} else if (movementY < -1 * TILES_GAP) {
 			moveTo((int) shape.getX(), (int) shape.getY() + TILES_SPEED_Y);
 			movementY += TILES_SPEED_Y;
-		}
-		else
-		{
-			if(movementX > 0)
+		} else {
+			if (movementX > 0)
 				moveTo((int) shape.getX() - TILES_GAP, (int) shape.getY());
-			else if(movementX < 0)
+			else if (movementX < 0)
 				moveTo((int) shape.getX() + TILES_GAP, (int) shape.getY());
-			else if(movementY > 0)
+			else if (movementY > 0)
 				moveTo((int) shape.getX(), (int) shape.getY() - TILES_GAP);
-			else if(movementY < 0)
+			else if (movementY < 0)
 				moveTo((int) shape.getX(), (int) shape.getY() + TILES_GAP);
 			isMoving = false;
 		}
