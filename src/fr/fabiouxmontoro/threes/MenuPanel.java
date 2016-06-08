34d730e6a8ccd35eaacc916de04648cb19901719
Nicolas-Montoro <nicolas.montoro@u-psud.fr>
2016-06-08 19:17:34 +0200

    Update MenuPanel.java

--- src/fr/fabiouxmontoro/threes/MenuPanel.java
+++ src/fr/fabiouxmontoro/threes/MenuPanel.java
@@ -9,7 +9,6 @@ import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
